cddabb93f73ef9eb2dbd5e9bce0a9e2d5000008b
Paul Russell <paulr711@gmail.com>
2024-09-30 21:26:35 +0000

    edited review form layout

--- reviews/forms.py
+++ reviews/forms.py
@@ -5,8 +5,9 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ["rating", "content", "user"]
+        fields = ["user", "rating", "content"]
         labels = {
+            "user": "Username",
             "rating": "Rate your experience",
             "content": "Write your review",
         }
