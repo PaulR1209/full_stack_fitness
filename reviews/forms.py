f8a96630c8dc89c83608e8c07770016a1d9f915d
Paul Russell <paulr711@gmail.com>
2024-09-30 15:10:38 +0000

    installed crispy forms to style review form

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,14 +1,16 @@
 from django import forms
 from .models import Review
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ["rating", "title", "content"]
-        widgets = {
-            "content": forms.Textarea(
-                attrs={"rows": 4, "placeholder": "Write your review here..."}
-            ),
-            "rating": forms.Select(attrs={"class": "form-select"}),
-        }
+
+    def __init__(self, *args, **kwargs):
+        super(ReviewForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = "POST"
+        self.helper.add_input(Submit("submit", "Submit Review"))
