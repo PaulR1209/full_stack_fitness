0ee9fafe31c20faf2c32e3844937390c1a51ddf8
Paul Russell <paulr711@gmail.com>
2024-10-09 14:49:03 +0000

    added membership model and view, and add to cart form

--- membership/models.py
+++ membership/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Membership(models.Model):
+    MEMBERSHIP_TYPES = (
+        ('Bronze', 'Bronze'),
+        ('Silver', 'Silver'),
+        ('Gold', 'Gold')
+    )
+
+    membership_type = models.CharField(max_length=100, choices=MEMBERSHIP_TYPES)
+    price = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.membership_type
--- membership/views.py
+++ membership/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
+from .models import Membership
 
 # Create your views here.
 
+
 def membership(request):
-    return render(request, 'membership/membership.html')
+    membership_bronze = Membership.objects.get(membership_type="Bronze")
+    membership_silver = Membership.objects.get(membership_type="Silver")
+    membership_gold = Membership.objects.get(membership_type="Gold")
+
+    return render(
+        request,
+        "membership/membership.html",
+        {
+            "membership_bronze": membership_bronze,
+            "membership_silver": membership_silver,
+            "membership_gold": membership_gold,
+        },
+    )
