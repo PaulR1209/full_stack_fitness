f29d0854820e60b7ad3d7717c51afed76eb5b9ce
Paul Russell <paulr711@gmail.com>
2024-10-11 15:28:50 +0000

    add checkout view and url

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import OrderForm
+from .models import Order
 
 # Create your views here.
+
+
+@login_required
+def checkout(request):
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.user = request.user
+            order.is_paid = False
+            order.save()
+            return redirect()
+    else:
+        form = OrderForm()
+    return render(request, "checkout/checkout.html", {"form": form})
