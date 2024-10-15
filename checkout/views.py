4089c962fb3b2e9a36d2d33375c097e88f0273e0
Paul Russell <paulr711@gmail.com>
2024-10-15 12:26:23 +0000

    added and styled stripe element and updated checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,42 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
+import stripe
 
 # Create your views here.
 
+
+stripe.api_key = settings.STRIPE_KEYS["SECRET_KEY"]
 
 @login_required
 def checkout(request):
     if request.method == "POST":
         form = OrderForm(request.POST)
+
         if form.is_valid():
-            order = form.save(commit=False)
-            order.user = request.user
-            order.save()
-            return redirect("membership")
+            try:
+                amount = 500
+                payment_intent = stripe.PaymentIntent.create(
+                    amount=amount,
+                    currency="gbp",
+                    payment_method_types=["card"],
+                )
+
+                client_secret = payment_intent['client_secret']
+            
+                order = form.save(commit=False)
+                order.user = request.user
+                order.stripe_pid = payment_intent.id
+                order.save()
+                messages.success(request, "Payment successful! You are now a member!")
+                return redirect("membership")
+        
+            except Exception as e:
+                return render(request, "checkout/checkout.html", {"form": form, "error": str(e)})
     else:
         form = OrderForm()
+
     return render(request, "checkout/checkout.html", {"form": form})
     
--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,21 @@
-// Stripe JS
-var stripe_publishable_key = $('#id_stripe_publishable_key').text().slice(1, -1);
-var stripe_secret_key = $('#id_stripe_secret_key').text().slice(1, -1);
-var stripe = Stripe(stripe_publishable_key);
-var elements = stripe.elements();
-var card = elements.create('card');
-var style = {
+// Stripe payment form
+document.addEventListener("DOMContentLoaded", function () {
+
+  var stripe = Stripe(
+    "pk_test_51Q8npkRo4WFpkduh4hmuGeoGfNevE4C63fyQU0NWkhtO8Zy0JHh78Imm05l1FtpUnaFNachbQiiI4yER1aT1ufL300OBML21aU"
+  );
+  var elements = stripe.elements();
+
+  var style = {
     base: {
-        color: '#000',
-        fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-        fontSmoothing: 'antialiased',
-        fontSize: '16px',
-        '::placeholder': {
-            color: '#aab7c4'
-        }
+      fontSize: "16px",
     },
     invalid: {
-        color: '#dc3545',
-        iconColor: '#dc3545'
-    }
-};
-var card = elements.create('card', {style: style});
-card.mount('#card-element');
+      color: "#dc3545",
+      iconColor: "#dc3545",
+    },
+  };
+
+  var card = elements.create("card", { style: style });
+  card.mount("#card-element");
+});
