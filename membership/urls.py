e755edee4bfa7ca2b0becad93119e48a25918da2
Paul Russell <paulr711@gmail.com>
2024-10-19 09:34:04 +0000

    restyled membership page, and added a manage membership page

--- membership/urls.py
+++ membership/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.membership, name='membership'),
+    path('manage/', views.manage, name='manage'),
 ]
--- membership/views.py
+++ membership/views.py
@@ -22,3 +22,36 @@ def membership(request):
             "user_order": user_order,
         },
     )
+
+
+def manage(request):
+    user_order = None
+    recurring_payment = None
+    payment_history = None
+
+    if request.user.is_authenticated:
+        user_order = Order.objects.filter(user=request.user).first()
+
+        if user_order:
+            # Get recurring payment and payment history related to the order
+            recurring_payment = user_order.recurring_payments.first()
+            payment_history = user_order.payment_history.all()
+
+    membership_bronze = Membership.objects.filter(membership_type="Bronze").first()
+    membership_silver = Membership.objects.filter(membership_type="Silver").first()
+    membership_gold = Membership.objects.filter(membership_type="Gold").first()
+
+    context = {
+        "user_order": user_order,
+        "recurring_payment": recurring_payment,
+        "payment_history": payment_history,
+        "membership_bronze": membership_bronze,
+        "membership_silver": membership_silver,
+        "membership_gold": membership_gold,
+    }
+
+    if user_order and user_order.is_cancelled:
+        context["reactivate"] = True  # Control to show Reactivate button
+
+    return render(request, "membership/manage.html", context)
+
