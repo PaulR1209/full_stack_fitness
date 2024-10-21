4e80fe3dcb93a822027624d276b9d9172f27ceee
Paul Russell <paulr711@gmail.com>
2024-10-21 15:25:10 +0000

    fix database url

--- full_stack_fitness/settings.py
+++ full_stack_fitness/settings.py
@@ -140,6 +140,9 @@ DATABASES = {
     'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
+print(DATABASES)
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
