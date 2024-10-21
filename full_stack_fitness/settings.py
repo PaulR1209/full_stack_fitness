133591b19b471d395452fe08618a4fbec4137343
Paul Russell <paulr711@gmail.com>
2024-10-21 15:48:07 +0000

    re added memberships to the database

--- full_stack_fitness/settings.py
+++ full_stack_fitness/settings.py
@@ -140,9 +140,6 @@ DATABASES = {
     'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
-print(DATABASES)
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
