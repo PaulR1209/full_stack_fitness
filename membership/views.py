746997ec59a6e3115caa79c9d5286a56db1e6082
Paul Russell <paulr711@gmail.com>
2024-10-22 13:44:06 +0000

    created and styled change membership page

--- membership/views.py
+++ membership/views.py
@@ -44,3 +44,25 @@ def manage(request):
     }
 
     return render(request, "membership/manage.html", context)
+
+
+def change_membership(request):
+    user_order = None
+
+    if request.user.is_authenticated:
+        user_order = (
+            Order.objects.filter(user=request.user).order_by("-created_at").first()
+        )
+
+    membership_bronze = Membership.objects.filter(membership_type="Bronze").first()
+    membership_silver = Membership.objects.filter(membership_type="Silver").first()
+    membership_gold = Membership.objects.filter(membership_type="Gold").first()
+
+    context = {
+        "user_order": user_order,
+        "membership_bronze": membership_bronze,
+        "membership_silver": membership_silver,
+        "membership_gold": membership_gold,
+    }
+
+    return render(request, "membership/change_membership.html", context)
