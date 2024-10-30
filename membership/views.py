d4bc1a460019ab2eca519206541f25420c10ce6b
Paul Russell <paulr711@gmail.com>
2024-10-30 16:26:14 +0000

    added membership view to view the users order

--- membership/views.py
+++ membership/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from .models import Membership
+from checkout.models import Order
 
 
 def membership(request):
     memberships = Membership.objects.all()
-    return render(request, 'membership/membership.html', {'memberships': memberships})
+    user_order = None
+    if request.user.is_authenticated:
+        user_order = Order.objects.filter(user=request.user).first()
+
+    return render(request, 'membership/membership.html', {
+        'memberships': memberships,
+        'user_order': user_order
+        })
