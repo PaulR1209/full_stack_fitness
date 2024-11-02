1487a4619c1d7c59d6b04ea8f7678ab0ff6e6c61
Paul Russell <paulr711@gmail.com>
2024-11-02 18:58:18 +0000

    update silver modal box template logic

--- home/views.py
+++ home/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-from checkout.views import sync_with_stripe
 
 # Create your views here.
 
 def index(request):
-    if request.user.is_authenticated:
-        sync_with_stripe(request.user)
     return render(request, 'home/index.html')
 
 
